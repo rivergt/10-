f804fba21c9731aa186eb2ee3e9d2653ee7411cc
rivergt <56811443+rivergt@users.noreply.github.com>
2019-10-29 10:46:04 +0800

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -4,7 +4,7 @@
 import math
 height = float(input('请输入您的身高，单位为米，例如1.75，按Enter确认:'))
 weight = float(input('请输入您的体重，单位为千克，例如70,按Enter确认:'))
-while height>0 and weight>0:
+if height>0 and weight>0:
     bmi = weight/(math.pow(height,2))
     if 0<=bmi<18.5:
          print ('您的身高是%.2f米，体重是%.2f千克，BMI指数为%.2f,过轻' % (height,weight,bmi))
@@ -16,5 +16,5 @@ while height>0 and weight>0:
          print ('您的身高是%.2f米，体重是%.2f千克，BMI指数为%.2f,肥胖' % (height,weight,bmi))
     else:
          print ('您的身高是%.2f米，体重是%.2f千克，BMI指数为%.2f,严重肥胖' % (height,weight,bmi))
-    break
-print('再见，欢迎下次测试！')
+else:
+    print('输入错误，欢迎下次测试！')
